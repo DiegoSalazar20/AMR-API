a58aaa48c3b9ad6a972aa36c7775b5a9bc4475fd
Raúl Álvarez Fernández <raulalvf@gmail.com>
2025-04-14 00:56:48 -0600

    US8 - Obtener ofertas según tipo de habitación.

--- AMR.API/Controllers/TipoHabitacionController.cs
+++ AMR.API/Controllers/TipoHabitacionController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AMR.RN.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AMR.API.Controllers
@@ -7,5 +8,19 @@ namespace AMR.API.Controllers
     [ApiController]
     public class TipoHabitacionController : ControllerBase
     {
-    }
+        private readonly ITipoHabitacionRN _tipoHabitacionRn;
+
+        public TipoHabitacionController(ITipoHabitacionRN tipoHabitacionRn)
+        {
+            _tipoHabitacionRn = tipoHabitacionRn;
+        }
+        
+        [HttpGet("ObtenerOfertas")]
+        public async Task<IActionResult> ObtenerOfertas()
+        {
+            var informacion = await _tipoHabitacionRn.ObtenerTarifas();
+            return Ok(informacion);
+        }//ObtenerOfertas
+
+    }//TipoHabitacionController
 }
--- AMR.DA/TipoHabitacionDA.cs
+++ AMR.DA/TipoHabitacionDA.cs
@@ -6,18 +6,19 @@ using System.Threading.Tasks;
 using AMR.DA.Contexto;
 using AMR.DA.Interfaces;
 using AMR.Dominio;
+using Microsoft.EntityFrameworkCore;
 
 namespace AMR.DA
 {
     public class TipoHabitacionDA : ITipoHabitacionDA
     {
-
         private readonly ContextoBD _context;
 
         public TipoHabitacionDA(ContextoBD context)
         {
             _context = context;
         }
+
         public Task<bool> ActualizarDatosHabiacion(string nombre, string descripcion, decimal tarifa, string imagen)
         {
             throw new NotImplementedException();
@@ -28,9 +29,43 @@ namespace AMR.DA
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<TipoHabitacion>> ObtenerTarifas()
+        public async Task<IEnumerable<TipoHabitacion>> ObtenerTarifas()
         {
-            throw new NotImplementedException();
-        }
+            var tipoHabitacionesEntidad = await _context.TipoHabitacion.ToListAsync();
+            var ofertasEntidad = await _context.Ofertas.ToListAsync();
+            DateTime fechaActual = DateTime.Today;
+
+            foreach (var tipoHabitacionEntidad in tipoHabitacionesEntidad)
+            {
+                tipoHabitacionEntidad.Precio +=
+                    tipoHabitacionEntidad.Precio * ObtenerDescuentoTemporada(fechaActual).Result;
+                foreach (var ofertaEntidad in ofertasEntidad)
+                {
+                    if (ofertaEntidad.IdTipoHabitacion == tipoHabitacionEntidad.IdTipoHabitacion &&
+                        ofertaEntidad.Fecha_inicio < fechaActual && ofertaEntidad.Fecha_final > fechaActual)
+                    {
+                        tipoHabitacionEntidad.Precio -=
+                            tipoHabitacionEntidad.Precio * ((decimal)ofertaEntidad.Descuento / 100);
+                    }
+                }
+            } //foreach externo
+
+            return tipoHabitacionesEntidad.Select(t => new TipoHabitacion
+            {
+                IdTipoHabitacion = t.IdTipoHabitacion,
+                Nombre = t.Nombre,
+                Descripcion = t.Descripcion,
+                Precio = t.Precio,
+                Imagen = t.Imagen
+            });
+        } //ObtenerTarifas
+
+        private async Task<decimal> ObtenerDescuentoTemporada(DateTime fechaActual)
+        {
+            var temporadaEntidad = await _context.Temporada.FirstOrDefaultAsync(
+                t => t.Fecha_inicio < fechaActual && t.Fecha_final > fechaActual
+            );
+            return temporadaEntidad != null ? ((decimal)temporadaEntidad.Descuento) / 100 : 0;
+        } //obtenerDescuentoTemporada
     }
 }
